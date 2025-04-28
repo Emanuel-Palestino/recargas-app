cdff3864c6a2612547da77094d433b023a467b88
Emanuel-Palestino <emanuel.palestino.h@gmail.com>
2025-04-27 21:21:21 -0600

    Add expo-contacts dependency and request permissions in RootLayout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,18 @@
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
+import { requestPermissionsAsync } from 'expo-contacts';
+
 
 export default function RootLayout() {
+  useEffect(() => {
+    (async () => {
+      const { status } = await requestPermissionsAsync();
+      if (status === 'granted') {
+
+      }
+    })();
+  }, []);
   return (
     <>
       <StatusBar style='dark' />
