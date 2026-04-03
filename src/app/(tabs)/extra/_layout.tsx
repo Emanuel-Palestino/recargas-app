1e6228d0f69b08e30c5a510b1b8c281b9244f1ca
Emanuel-Palestino <emanuel.palestino.h@gmail.com>
2026-04-03 15:01:04 -0600

    feat: add custom header for extra pages and update expo dependencies

--- src/app/(tabs)/extra/_layout.tsx
+++ src/app/(tabs)/extra/_layout.tsx
@@ -1,10 +1,32 @@
+import { ThemedView } from "@/components/ThemedView";
+import BackIcon from "@/icons/BackIncon";
+import { Spacing } from "@/constants/theme";
 import { Stack } from "expo-router";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function ExtraLayout() {
   return (
     <Stack
       screenOptions={{
         headerShadowVisible: false,
+        // Custom header needs to be implemented due to a bug in expo, default header is not inside the safe area
+        // TODO: Remove this when the bug is fixed
+        header: ({ navigation, route, options }) => {
+          const title = options.title ?? route.name;
+          return (
+            <ThemedView>
+              <SafeAreaView edges={['left', 'top', 'right']} style={{}}>
+                <View style={styles.headerContainer}>
+                  <Pressable onPress={() => navigation.goBack()} style={styles.backButton}>
+                    <BackIcon />
+                  </Pressable>
+                  <Text style={styles.headerTitle}>{title}</Text>
+                </View>
+              </SafeAreaView>
+            </ThemedView>
+          );
+        },
       }}
     >
       <Stack.Screen name="index" options={{ headerShown: false }} />
@@ -13,3 +35,21 @@ export default function ExtraLayout() {
     </Stack>
   );
 }
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingHorizontal: Spacing.three,
+    paddingVertical: Spacing.two,
+  },
+  backButton: {
+    marginRight: Spacing.three,
+    padding: Spacing.two,
+  },
+  headerTitle: {
+    fontSize: 18,
+    lineHeight: 20,
+    fontWeight: "bold",
+  },
+})
